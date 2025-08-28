c839c7270651b700a91f0bf1097132e7a7b8fb63
Saswat Pal <saswat2001@gmail.com>
2025-08-28 23:51:08 +0530

    implement api gateway

---
+++ api-gateway/src/test/java/com/saswat/ShopPal/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saswat.ShopPal.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
