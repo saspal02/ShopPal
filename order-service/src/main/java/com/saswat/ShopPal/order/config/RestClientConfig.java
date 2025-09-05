afe73de0862794b077035c994afc52352c64f0e6
Saswat Pal <saswat2001@gmail.com>
2025-09-05 23:15:23 +0530

    add circuit breakers

--- order-service/src/main/java/com/saswat/ShopPal/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/saswat/ShopPal/order/config/RestClientConfig.java
@@ -2,11 +2,16 @@ package com.saswat.ShopPal.order.config;
 
 import com.saswat.ShopPal.order.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.http.client.ClientHttpRequestFactoryBuilder;
+import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -18,10 +23,24 @@ public class RestClientConfig {
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(getClientRequestFactory())
                 .build();
+
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
 
     }
+
+    private ClientHttpRequestFactory getClientRequestFactory() {
+       ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.defaults()
+               .withConnectTimeout(Duration.ofSeconds(3))
+               .withReadTimeout(Duration.ofSeconds(3));
+       return ClientHttpRequestFactoryBuilder.detect().build(clientHttpRequestFactorySettings);
+    }
+
+
+
+
+
 }
