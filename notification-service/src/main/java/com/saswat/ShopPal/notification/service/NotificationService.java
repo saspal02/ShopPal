46cb1a5b43e17c5fa3d640f6a73fb444a845dcb7
Saswat Pal <saswat2001@gmail.com>
2025-09-11 22:36:04 +0530

    add kafka functionality

---
+++ notification-service/src/main/java/com/saswat/ShopPal/notification/service/NotificationService.java
@@ -0,0 +1,49 @@
+package com.saswat.ShopPal.notification.service;
+
+import com.saswat.ShopPal.notification.order.event.OrderPlacedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationService {
+
+    private final JavaMailSender javaMailSender;
+
+    @KafkaListener(topics="order-placed")
+    public void listen(OrderPlacedEvent orderPlacedEvent) {
+        log.info("Got message from order-placed topic: {}", orderPlacedEvent);
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("shoppal@gmail.com");
+            messageHelper.setTo(orderPlacedEvent.getEmail());
+            messageHelper.setSubject(String.format("Your Order with OrderNumber %s is placed successfully", orderPlacedEvent.getOrderNumber()));
+            messageHelper.setText(String.format("""
+                    Hi,
+                    
+                    Your order with order number %s is placed successfully.
+                    Best Regards
+                    ShopPal 
+                    """,
+                    orderPlacedEvent.getOrderNumber()));
+
+        };
+        try {
+            javaMailSender.send(messagePreparator);
+            log.info("Order Notification email sent!!");
+        } catch (MailException e) {
+            log.error("Exception occured when sending mail", e);
+            throw new RuntimeException("Exception occured when sending mail to " + orderPlacedEvent.getEmail());
+
+        }
+    }
+
+
+}
--- order-service/src/main/java/com/saswat/ShopPal/order/service/OrderService.java
+++ order-service/src/main/java/com/saswat/ShopPal/order/service/OrderService.java
@@ -2,19 +2,25 @@ package com.saswat.ShopPal.order.service;
 
 import com.saswat.ShopPal.order.client.InventoryClient;
 import com.saswat.ShopPal.order.dto.OrderRequest;
+import com.saswat.ShopPal.order.event.OrderPlacedEvent;
 import com.saswat.ShopPal.order.model.Order;
 import com.saswat.ShopPal.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String,OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
         var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
@@ -22,11 +28,17 @@ public class OrderService {
         if (isProductInStock) {
             Order order = new Order();
             order.setOrderNumber(UUID.randomUUID().toString());
-            order.setPrice(orderRequest.price());
+            order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
             order.setSkuCode(orderRequest.skuCode());
             order.setPrice(orderRequest.price());
             order.setQuantity(orderRequest.quantity());
             orderRepository.save(order);
+
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(),orderRequest.userDetails().email());
+            log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order_placed",orderPlacedEvent);
+            kafkaTemplate.send("order_placed",orderPlacedEvent);
+            log.info("End - Sending OrderPlacedEvent {} to Kafka topic order_placed",orderPlacedEvent);
+
         } else {
             throw new RuntimeException("Product with SkuCode" + orderRequest.skuCode() + " is not in stock");
         }
