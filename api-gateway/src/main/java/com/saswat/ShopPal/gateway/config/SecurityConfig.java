c35802c47c67f25feccf2aae3f16524aab1b2fb0
Saswat Pal <saswat2001@gmail.com>
2025-09-22 00:09:41 +0530

    refactor and clean up unused code

--- api-gateway/src/main/java/com/saswat/ShopPal/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/saswat/ShopPal/gateway/config/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
-            "/swagger-resources/**", "/api-docs/**", "/aggregate/**"};
+            "/swagger-resources/**", "/api-docs/**", "/aggregate/**","/actuator/prometheus"};
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(authorize -> authorize
